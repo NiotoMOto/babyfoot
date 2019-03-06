84f67aa4d0b8a38e49c723adafd15e3af40f14ee
Antoine Guillemoto <antoine.guillemoto@gmail.com>
2019-03-06 14:55:30 +0100

    feat(header) add logout

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import { auth } from "./firebaseConfig";
 
 export function Header() {
   return (
     <AppBar color="primary" position="relative">
       <Toolbar>
-        <Typography style={{ color: "white" }} variant="h5">
+        <Typography style={{ color: "white", flexGrow: 1 }} variant="h5">
           Baby-foot
         </Typography>
+        <Button
+          onClick={() =>
+            auth()
+              .signOut()
+              .then(() => window.location.reload())
+          }
+          color="inherit">
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
