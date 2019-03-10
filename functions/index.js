c0da67e2f2bd1c886df06baf5c615ffafa1c0120
Antoine Guillemoto <antoine.guillemoto@gmail.com>
2019-03-10 18:05:36 +0100

    feat(functions) add wins

--- functions/index.js
+++ functions/index.js
@@ -64,3 +64,42 @@ exports.addStats = functions.firestore
       })
       .catch(err => err);
   });
+
+const labelPosition = {
+  0: "first",
+  1: "second",
+  2: "third"
+};
+
+function storeWeeksStats(stats) {
+  return Promise.all(
+    _.take(_.orderBy(stats, ["spoint"], ["desc"]), 3).map((stat, index) => {
+      return stat.docRef.get().then(user => {
+        const userData = user.data();
+        const fieldToUpdate = `wins.${labelPosition[index]}`;
+        console.log(
+          userData.wins,
+          fieldToUpdate,
+          _.get(userData, fieldToUpdate),
+          {
+            [fieldToUpdate]: add(_.get(userData, fieldToUpdate), 1)
+          }
+        );
+        return stat.docRef.update({
+          [fieldToUpdate]: add(_.get(userData, fieldToUpdate), 1)
+        });
+      });
+    })
+  );
+}
+
+exports.addWins = functions.firestore
+  .document("weeks/{weekId}")
+  .onCreate((snap, context) => {
+    const statsData = snap.data();
+    return Promise.all([storeWeeksStats(statsData.stats)])
+      .then(() => {
+        return "Send OK";
+      })
+      .catch(err => err);
+  });
