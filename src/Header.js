ae3e3945c18f53934db52a68182d2ee1db7b40bb
Antoine Guillemoto <antoine.guillemoto@gmail.com>
2019-03-11 15:04:39 +0100

    fix(header) remove drawer

--- src/Header.js
+++ src/Header.js
@@ -4,65 +4,18 @@ import {
   Toolbar,
   Typography,
   Button,
-  IconButton,
-  Drawer,
-  List,
-  Divider
+  IconButton
 } from "@material-ui/core";
 import { auth } from "./firebaseConfig";
 import { Link } from "react-router-dom";
 import MenuIcon from "@material-ui/icons/Menu";
-import { ListItem } from "semantic-ui-react";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/Notifications";
-import { askForPermissioToReceiveNotifications } from "./pushNotification";
-
-function Menu() {
-  return (
-    <div style={{ width: "250px" }}>
-      <List style={{ marginLeft: "10px" }}>
-        <ListItem
-          style={{
-            width: "100%",
-            display: "flex",
-            position: "relative",
-            boxSizing: "border-box",
-            textAlign: "left",
-            alignItems: "center",
-            paddingTop: "11px",
-            paddingBottom: "11px",
-            justifyContent: "flex-start",
-            textDecoration: "none"
-          }}
-          onClick={askForPermissioToReceiveNotifications}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Notifications" />
-        </ListItem>
-      </List>
-    </div>
-  );
-}
 
 export function Header() {
   const [menu, setMenu] = useState(false);
   return (
     <Fragment>
-      <Drawer open={menu} onClose={() => setMenu(false)}>
-        <div tabIndex={0} role="button">
-          <Menu />
-        </div>
-      </Drawer>
       <AppBar color="primary" position="relative">
         <Toolbar>
-          <IconButton
-            onClick={() => setMenu(true)}
-            color="inherit"
-            aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
           <Typography style={{ color: "white", flexGrow: 1 }} variant="h5">
             <Link style={{ textDecoration: "none", color: "white" }} to="/">
               Baby-foot
